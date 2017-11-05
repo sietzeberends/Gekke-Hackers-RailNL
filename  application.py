d94c19802ee1837a1d32a2f7d1d9439d713339eb
Sietze Berends <sietze.berends@gmail.com>
2017-11-05 13:49:56 +0100

    Created application file & station class

---
+++  application.py
@@ -0,0 +1,4 @@
+import Station
+
+a = Station("Enschede", 52.21833, 6.89583, false)
+print(a.str())
---
+++ station.py
@@ -0,0 +1,10 @@
+class Station:
+	def __init__(self, name, latitude, longitude, critical):
+		self.name = name
+		self.latitude = latitude
+		self.longitude = longitude
+		self.critical = critical
+
+	def __str__():
+		return self.name + ", " + self.latitude + ", " + self.longitude + ", "
+		+ critical
