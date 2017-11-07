a998643bc09643188b23d986ba0a0dd7267e9ebb
Sietze Berends <sietze.berends@gmail.com>
2017-11-07 13:18:35 +0100

    Station class aangepast
    Station bevat nu alle mogelijke connecties voor dat specifieke station

--- Classes/trajectory.py
+++ Classes/trajectory.py
@@ -2,22 +2,19 @@
 # Trajectory has a name and a list of connections
 
 class Trajectory:
-	def __init__(self, connections):
-
+	def __init__(self, name, connections):
+		self.name = name
 		self.connections = connections
-		self.time = 0
-		for connection in connections:
-			self.time += connection.time
 
 	def __str__(self):
 		output = ""
 		time = 0
 		for connection in self.connections:
 			 output += connection.station1
-			 output += " -> "
+			 output += " - "
+			 output += connection.station2
+			 output += "; "
+			 time += connection.time
 
-		last_station = self.connections[-1]
-		output += last_station.station2
-
-		output += " total time: " + str(self.time) + " minutes"
+		output += "total time: " + str(time) + " minutes"
 		return output
--- application.py
+++ application.py
@@ -1,9 +1,7 @@
 from station import Station
 from connection import Connection
 from trajectory import Trajectory
-from datetime import datetime
 
-import random
 import csv
 
 # track all stations and connections in two lists
@@ -30,49 +28,19 @@ for station in stations:
 
 print stations[0].getConnections()
 
-
 # let's test if our connections can be put in an instance of the class 'Trajectory'
 connectionsForFirstTrajectory = []
 
 # we use connections to create a trajectory between Amsterdam Amstel and Haarlem
 # TODO: create a function to generate a random traject
 # of course, connections need to be linked to eachother
-# y = random.choice(connections)
-# def addconnection(y):
-# 	y = random.choice(connections)
-# 	if len(connectionsForFirstTrajectory) > 10:
-# 		return connectionsForFirstTrajectory
-# 	else:
-# 		for connection in connections:
-# 			if connection.station2 == y.station1:
-# 				return addconnection(connectionsForFirstTrajectory.extend((connection, y)))
-
-# store time
-startTime = datetime.now()
-
-def addconnection():
-    connectionsForFirstTrajectory.append(random.choice(connections))
-    while len(connectionsForFirstTrajectory) < 5:
-        x = random.choice(connections)
-        if x.station1 == connectionsForFirstTrajectory[-1].station2:
-          connectionsForFirstTrajectory.append(x)
-
-# while connectionsForFirstTrajectory[7] == False:
-addconnection()
-#
-# for connection in connections:
-# 	x = random.choice(connections)
-# 	if (y.station2 == x.station1 and y.time < (120 - x.time)):
-# 		connectionsForFirstTrajectory.extend((y, x))
+connectionsForFirstTrajectory.extend((connections[2], connections[7], connections[6]))
 
 # now, let's build the first trajectory. Right now, the name is manually given
 # TODO: create a function to create the name of the trajectory
 # based on the first station of the first connection in the trajectory
 # and the last station of the last connection in the trajectory
-firstTrajectory = Trajectory(connectionsForFirstTrajectory)
-
-timeElapsed = datetime.now()-startTime
-print('Time elapsed (hh:mm:ss.ms) {}'.format(timeElapsed))
+firstTrajectory = Trajectory("Amsterdam Amstel -> Haarlem", connectionsForFirstTrajectory)
 
 # check if it worked
 print firstTrajectory
