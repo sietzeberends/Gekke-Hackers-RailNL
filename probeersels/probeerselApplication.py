eb3350ff8486ead206721c7f4367efdce05b5a6e
fholstege <floris.figo@gmail.com>
2017-11-20 16:21:57 +0100

    added two html files - one for complete map, and one for the solution of trajectories

--- probeersels/probeerselApplication.py
+++ probeersels/probeerselApplication.py
@@ -87,6 +87,16 @@ for j in range(1,100):
 			for trajectory in testHillClimber.trajectories:
 				besteLijnvoering.trajectories.append(trajectory)
 			print(str(besteLijnvoering))
+			with open ("connections_visualisation.csv", "w") as outfile:
+				writer = csv.writer(outfile, dialect='excel')
+				for trajectory in besteLijnvoering.trajectories:
+					writer.writerow("-")
+					for connection in trajectory.connections:
+						placeholder = connection.station1.name + ", " +\
+		 			  				  connection.station2.name + ", " +\
+				      				  str(connection.time)
+						placeholder = placeholder.split(",")
+						writer.writerow(placeholder)
 
 
 
