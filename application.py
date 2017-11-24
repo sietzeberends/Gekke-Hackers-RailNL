7f1e65f81905621dc82ea359c15aa5582946cf59
Sietze Berends <sietze.berends@gmail.com>
2017-11-24 12:58:09 +0100

    Merge branch 'master' of https://github.com/sietzeberends/Gekke-Hackers-RailNL

--- application.py
+++ application.py
@@ -1,3 +1,4 @@
+<<<<<<< current
 from Classes.station import Station
 from Classes.connection import Connection
 from Classes.trajectory import Trajectory
@@ -113,3 +114,87 @@ print ("Totale tijd van lijnvoering: " + str(besteLijnvoering.time))
 # print the runtime
 timeElapsed = datetime.now()-startTime
 print('Time elapsed (hh:mm:ss.ms) {}'.format(timeElapsed))
+=======
+from Classes.station import Station
+from Classes.connection import Connection
+from Classes.trajectory import Trajectory
+from datetime import datetime
+
+import csv
+import random
+
+startTime = datetime.now()
+
+# track all stations and connections in two lists
+stations = []
+connections = []
+
+
+# load all the stations
+with open('csvFiles/StationsHolland.csv', 'r') as csvfile:
+	rows = csv.reader(csvfile)
+	for row in rows:
+		stations.append(Station(row[0], row[1], row[2], row[3]))
+
+# load all the connections
+with open('csvFiles/ConnectiesHolland.csv', 'r') as csvfile:
+	rows = csv.reader(csvfile)
+	for row in rows:
+		connections.append(Connection(Station(row[0], "", "", row[3]),
+									  Station(row[1], "", "", row[3]),
+									  row[2],
+									  row[3]))
+# add the children to the gconnections
+for connection in connections:
+	connection.addChildren(connections)
+
+count = 0
+stationsk = []
+for connection in connections:
+	if connection.critical == "TRUE":
+	  stationsk.append(connection.station1.name)
+	  stationsk.append(connection.station2.name)
+for Station.name in stations:
+	if str(Station.name) in stationsk:
+		count += 1
+		print (count)
+if count == 22:
+	print ("POOF IT IS PROOF QED")
+else:
+	print ("not a PROOF")
+
+connectionsForTrajectory =[]
+
+# # create a random Trajectory
+# def createTrajectory(connection, time):
+# 	connectionsForTrajectory.append(connection)
+# 	time += connection.time
+# 	# as long as the Trajectory has a lower duration than 120
+# 	while True:
+# 		# if the station only has one child (i.e. is on the edge of civilization)
+# 		if len(connection.connections) == 1:
+# 			newConnection = connection.connections[0]
+# 			newConnection.addChildren(connections)
+# 		# if the station has more than one child
+# 		else:
+# 			newConnection = random.choice(connection.connections[0:-1])
+# 			newConnection.addChildren(connections)
+# 		# make sure we don't overstep our constraint
+# 		if newConnection.time + time < 120:
+# 			# recursive
+# 			return createTrajectory(newConnection, time)
+# 		else:
+# 			break
+
+time = 0
+createTrajectory(random.choice(connections), time)
+
+
+firstTrajectory = Trajectory(connectionsForTrajectory)
+
+# check if it worked
+print (firstTrajectory)
+
+timeElapsed = datetime.now()-startTime
+print('Time elapsed (hh:mm:ss.ms) {}'.format(timeElapsed))
+>>>>>>> before discard
