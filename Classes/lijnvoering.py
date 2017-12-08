1f15d58c4e4f5aaa9e2fe5ad12c8175f7ac0854c
Sietze Berends <sietze.berends@gmail.com>
2017-12-08 10:26:05 +0100

    depthfirst weer aangepast. trololo

--- Classes/lijnvoering.py
+++ Classes/lijnvoering.py
@@ -200,7 +200,9 @@ class LijnVoering:
                 connection = stack.pop()
             # if the previous connection was added, pop a new one
             elif allIsWell:
+                print("from stack: " + str(stack[-1]))
                 connection = stack.pop()
+                print("to connection: " + str(connection))
             # if the previous connection was not added, we already popped a new one
             else:
                 print("Keep going...")
@@ -247,13 +249,19 @@ class LijnVoering:
             # 1. if it's in the dict, pop the next one
             if inDict:
                 print("Already in dict, pop the next one")
+                print("to throw away: " + str(connection))
                 connection = stack.pop()
                 allIsWell = False
 
             # 2. if the time is going to exceed 120 minutes, pop the next one
             elif exceedsTime and not levelUp:
                 print("Will exceed time, pop the next one")
+                print("to throw away: " + str(connection))
+                for connection in stack:
+                    print("STACK PRINTEN")
+                    print(connection)
                 connection = stack.pop()
+                print(connection)
                 allIsWell = False
 
             # 3. if the time exceeds but it's from another level, append after going back to that level
@@ -284,6 +292,8 @@ class LijnVoering:
                     shadowTrajectory.time += c.time
                 allTrajectories.append(shadowTrajectory)
                 dictTrajectory[stringKey] = True
+                print("trajectory for next round: " + str(trajectory))
+
 
                 # edge of civilization check
                 if len(connection.children) == 1:
@@ -291,6 +301,7 @@ class LijnVoering:
                     print(self.connections[self.connections[connection.children[0]].index])
                     print("edge of civilization, bounce allowed")
                     stack.append(self.connections[self.connections[connection.children[0]].index])
+
 
                 for tconnection in trajectory.connections:
                     if connection.station2.name == tconnection.station1.name and len(trajectory.connections[0].children) != 1:
@@ -304,22 +315,50 @@ class LijnVoering:
                     for child in connection.children:
                         # if it's a bounce, don't add it to the stack
                         if connection.station1.name == self.connections[child].station2.name:
+                            print("")
                             print(self.connections[child])
                             print("invalid bounce, don't add to stack")
                             # if it's not a bounce, add it to the stack
                         else:
                             print(self.connections[child])
-                            print("add to stack")
+                            print("add to stack: " + str(self.connections[child]))
                             stack.append(self.connections[child])
+
+                if root == 55:
+                    break
+
                 allIsWell = True
-                print("trajectory for next round: " + str(trajectory))
+
             n += 1
 
-        print(n)
+
+        if len(stack) == 0:
+            level = 1
+            for tconnection in reversed(trajectory.connections):
+                if connection.station1.name == tconnection.station1.name:
+                    break
+                else:
+                    level += 1
+
+            for j in range(0, level):
+                if len(trajectory.connections) > 1:
+                    x = trajectory.connections.pop()
+                    trajectory.time -= x.time
+            if connection.time + trajectory.time <= 120:
+                trajectory.connections.append(connection)
+                trajectory.time += connection.time
+
+            shadowTrajectory = Trajectory()
+            for c in trajectory.connections:
+                shadowTrajectory.connections.append(c)
+                shadowTrajectory.time += c.time
+            allTrajectories.append(shadowTrajectory)
+
         root += 1
 
         if root < 56:
             return self.depthFirstSearch(root, n, allTrajectories)
+
 
         else:
             bestLijnvoering = LijnVoering(self.csvFilepath)
@@ -343,6 +382,7 @@ class LijnVoering:
             print(highScore)
             print("Amount of critical connections: " + str(bestLijnvoering.kritiekInLijnvoering))
             print (len(allTrajectories))
+            print(n)
 
     def ScoreOpdrachtA(self):
         for connection in connections:
