45477c895bf72c3fed70bfb73abcd2fdd2a00ca8
daanuittenhout <daanuittenhout@gmail.com>
2017-12-18 10:53:00 +0100

    ok

--- Algorithms/plot.py
+++ Algorithms/plot.py
@@ -2,23 +2,45 @@ from matplotlib import pyplot as plt
 import numpy as np
 import csv
 import matplotlib.cbook as cbook
-
+import math
+import matplotlib.mlab as mlab
 x = []
 y = []
 z = []
 
+x1 = []
+y1 = []
+z1 = []
 
-with open ("C:/Users/User4/Documents/GitHub/Gekke-Hackers-RailNL/Experiments/HC-Critical-5-160000-NAT.csv", "r") as csvfile:
+
+with open ("Experiments/SA-500-1600-HARDCODED.csv", "r") as csvfile:
     plots = csv.reader(csvfile, delimiter =",")
     for row in plots:
         x.append(int(row[2]))
         z.append(int(row[0]))
         y.append(float(row[1]))
+with open ("Experiments/SA-5-160000-T1250-HARDCODED.csv", "r") as csvfile:
+    plots = csv.reader(csvfile, delimiter =",")
+    for row in plots:
+        x1.append(int(row[2]))
+        z1.append(int(row[0]))
+        y1.append(float(row[1]))
+
 
 # Plot...
 fig = plt.figure()
 ax1 = fig.add_subplot(111)
+ax2 = fig.add_subplot()
 
+yBar = np.mean(y)
+yVar = np.var(y)
+yS = yVar ** 0.5
+n = len(y)
+
+yBarAlternative = np.mean(y)
+yVarAlternative = np.var(y)
+ySAlternative = yVar ** 0.5
+nalternative = len(y)
 
 
 ymax = max(y)
@@ -26,25 +48,26 @@ xpos = y.index(ymax)
 xmax = x[xpos]
 zmax = z[xpos]
 
-ax1.annotate( str(ymax), xy=(xmax, ymax), xytext=(xmax, ymax+20),
+ax1.annotate( str(yBar), xy=(yBar, 0.000225), xytext=(yBar, 0.000225),
 arrowprops=dict(facecolor='yellow', shrink=0.01),
 )
 
 
 ax = plt.gca()
 ax.set_facecolor('blue')
-ax.set_facecolor((1 , 1, 1))
-plt.scatter(x, y, label = "Lijnvoering", c=z, s=5)
+ax.set_facecolor((1, 0, 0))
+# plt.scatter(x, y, label = "Lijnvoering", c=z, s=5)
 
-
-plt.gray()
-print(x)
-plt.xlabel("minutes")
-plt.ylabel("score")
-plt.title("HC-Critical-5-160000-NAT")
-plt.legend()
-cbar = plt.colorbar()
-cbar.set_label('# Trajectories')
+plt.plot(y,mlab.normpdf(y, yBar, yS))
+#
+# plt.gray()
+# print(x)
+# plt.xlabel("minuten")
+# plt.ylabel("score")
+# plt.title("SBG")
+# plt.legend()
+# cbar = plt.colorbar()
+# cbar.set_label('# trajecten')
 
 # use_colours = {"1": "red", "2": "green", "3": "blue", "4": "purple", "5": "yellow", "6": "Black", "7" : "grey"}
 # if z > 2:
@@ -52,7 +75,8 @@ cbar.set_label('# Trajectories')
 # else:
 #     plt.scatter(x,y, label = 'min', color = 'green', s =5)
 
-print(y)
+print((yS))
+print (yBar)
 # plt.plot(x,y)
 plt.show()
 #
