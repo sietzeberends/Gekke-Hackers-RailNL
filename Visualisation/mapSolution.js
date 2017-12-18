341137ebc1c3ba3d5da2be329724a007f07f43b5
fholstege <floris.figo@gmail.com>
2017-12-18 17:39:02 +0100

    added files

--- Visualisation/mapSolution.js
+++ Visualisation/mapSolution.js
@@ -1,11 +1,10 @@
-width_map = 1000
-height_map = 1200
+widthMap = 1000
+heightMap = 1200
 
 // when scripts loaded, implement functions that require d3 and tooltip
 window.onload = function(d){
 
-
-  // create tooltip for stations
+// create tooltip for stations
 var tipStations = d3.tip()
   .attr('class', 'd3-tip')
   .offset([-10, 0])
@@ -18,45 +17,121 @@ var tipConnections = d3.tip()
   .attr('class', 'd3-tip')
   .offset([-10,0])
   .html(function(d){
-      return "<span>" + "Connection: " + d.station1 + " -> " + d.station2 + "</span>";
+      return "<span>" + "Connection: " + 
+      d.station1 + " -> " + d.station2 + 
+      "</span>";
   })
 
 
  // create svg for scatterplot
-
 var map = d3.select("body").append("svg")
-    .attr({"width": width_map,
-           "height": height_map})
+    .attr({"width": widthMap,
+           "height": heightMap})
     .append("g");
 
+// append svg of the map of the Netherlands
 var img = map.append("svg:image")
-    .attr("xlink:href", "Blank_map_of_the_Netherlands.svg")
-    .attr("width", width_map)
-    .attr("height", height_map)
+    .attr("xlink:href", "blankMapOfTheNetherlands.svg")
+    .attr("width", widthMap)
+    .attr("height", heightMap)
     .attr("x", 0)
     .attr("y", 0);
 
+// call tooltips to map
 map.call(tipStations);
 map.call(tipConnections);
 
 
 // create scale for X axis
 var scale_x = d3.scale.linear()
-    .range([110, width_map - 90])
+    .range([110, widthMap - 90])
 
 // create scale for Y axis
 var scale_y = d3.scale.linear()
-    .range([height_map - 90, 175])
+    .range([heightMap - 90, 175])
 
 
-locations_stations = {}
+var locations_stations = {}
+
+// create menu to select country
+var button = d3.select("body").append("div")
+    .attr("class", "menu")
+  
+button
+  .append("button")
+    .attr("type", "button")
+    .attr("class", "btn btn-primary dropdown-toggle")
+    .attr("data-toggle", "dropdown")
+    .text("Pick a solution!")
+    .append("span")
+    .attr("class", "caret")
+
+
+// queue 
+var q = d3.queue()
+  .defer(d3.json, "StationsNationaal.json")
+  .defer(d3.json, "Greedy_4.json")
+  .defer(d3.json, "HillclimberNoordZuid.json")
+  .defer(d3.json, "HillclimberNationaal.json")
+  .await(makemap);
+
+  function createSolution (data){
+
+  var counter = 0
+  var colours = ["placeholder","red","#ff8000","#ffff00","#40ff00", 
+  				 "#00ffff", "#0000ff", "#bf00ff", "#ff00ff", "#black", 
+  				 "#fffff0", "#008B8B", "#A52A2A", "#006400", "#BDB76B"]
+  var connectionCoordinates = []
+
+  var lines = map.selectAll("connection")
+  .data(data)
+    .enter().append("line")
+    .transition()
+    .delay(function(d,i){ return i * 50})
+    .attr('class', 'connection')
+    .attr('x1', function(d){if (d.nextTrajectory=="False")
+                            {
+                              return scale_x(locations_stations[d.station1][1]);
+                            }
+                          })
+    .attr('x2', function(d){ if (d.nextTrajectory=="False")
+                            {
+                              return scale_x(locations_stations[d.station2.trim()][1]);
+                            }
+                          })
+    .attr('y1', function(d){ if (d.nextTrajectory=="False")
+                            {
+                              return scale_y(locations_stations[d.station1.trim()][0]);
+                            }
+                          })
+    .attr('y2', function(d){ if (d.nextTrajectory=="False")
+                            {
+                              return scale_y(locations_stations[d.station2.trim()][0]);
+                            }
+                          })
+  .attr("stroke-width", 2)
+  .attr("stroke", function(d){ if (d.nextTrajectory=="True")
+                               {
+                                 counter = counter + 1
+                               }
+                              return colours[counter]
+                             })
+  }
+
+
+
+
     
 
-d3.json("StationsNationaaltest.json", function(data){
+function makemap(error, stations, Greedy, HillclimberNoordZuid, HillclimberNationaal){
+  if (error) throw error;
 
+console.log(Greedy)
+console.log(HillclimberNoordZuid)
+console.log(HillclimberNationaal)
 
 // save locations of stations to list
-data.forEach(function(d){
+stations.forEach(function(d){
 
 	locations_stations[String(d.station)] = [d.latitude, d.longitude, d.critical]
 })
@@ -64,14 +139,14 @@ data.forEach(function(d){
 
 // change domain
 scale_x
-.domain(d3.extent(data,function(d){return d.longitude;})).nice();
+.domain(d3.extent(stations,function(d){return d.longitude;})).nice();
 
 scale_y
-.domain(d3.extent(data,function(d){return d.latitude;})).nice();
+.domain(d3.extent(stations,function(d){return d.latitude;})).nice();
 
 // create circles 
 var circles = map.selectAll("dot")
-    .data(data)
+    .data(stations)
     .enter().append("circle")
     .attr({
       "class":"dot", 
@@ -95,82 +170,38 @@ var circles = map.selectAll("dot")
               tipStations.hide(d);
           })
 
-})
 
 
-d3.json("connections_visualisation.json", function(data){
+var solutions = ["Greedy", "Hillclimber - NoordZuid", "Hillclimber - Nationaal"]
 
-var counter = 0
-var width = 2
-var colours = ["placeholder","green","red","pink","yellow"]
-var connectionCoordinates = []
-var adjustment = 2
+// create dropdown menu when button is pressed
+var menu = button.append("ul")
+    .attr("class", "dropdown-menu")
+    .attr("role", "menu")
+// create dropdown menu for button
+  menu.selectAll("li")
+      .data(solutions)
+      .enter().append("li")
+          .append("a")
+          .attr("class", "m")
+          .attr("href", "#")
+          .text(function(d){ return d})
+          .attr("value", function(d){ return d})
+          .on("click", function(d){
+            d3.selectAll(".connection").remove()
+            var solution = this.getAttribute("value")
 
+            if (solution == "Hillclimber - NoordZuid"){
+              createSolution(HillclimberNoordZuid)
+            }
+            else if (solution == "Greedy"){
+              createSolution(Greedy)
+            }
+            else if (solution == "Hillclimber - Nationaal"){
+              createSolution(HillclimberNationaal)
+            }
+          })
 
-var lines = map.selectAll("connection")
-  .data(data)
-    .enter().append("line")
-    .transition()
-    .delay(function(d,i){ return i * 200})
-    .attr('class', 'connection')
-    .attr('x1', function(d){if (d.nextTrajectory=="False")
-                              {
-                                if (connectionCoordinates.includes(String(d.station1 + d.station2), String(d.station2 + d.station1)))
-                                {
-                                  return scale_x(locations_stations[d.station1][1]) + adjustment;
-                                }
-                                else
-                                { 
-                                  return scale_x(locations_stations[d.station1][1]);
-                                }
-                              }
-
-                           })
-    .attr('x2', function(d){if (d.nextTrajectory=="False")
-                              {
-                                if (connectionCoordinates.includes(String(d.station1 + d.station2), String(d.station2 + d.station1)))
-                                {
-                                  return scale_x(locations_stations[d.station2.trim()][1]) + adjustment;
-                                }
-                                else
-                                {
-                                  return scale_x(locations_stations[d.station2.trim()][1]);
-                                }
-                              }
-                           })
-    .attr('y1', function(d){if (d.nextTrajectory=="False")
-                              {
-                                if (connectionCoordinates.includes(String(d.station1 + d.station2), String(d.station2 + d.station1)))
-                                {
-                                  return scale_y(locations_stations[d.station1.trim()][0]) + adjustment;
-                                }
-                                else
-                                {
-                                  return scale_y(locations_stations[d.station1.trim()][0]);
-                                }
-                              }
-                           })
-    .attr('y2', function(d){if (d.nextTrajectory=="False")
-                              {
-                                if (connectionCoordinates.includes(String(d.station1 + d.station2), String(d.station2 + d.station1)))
-                                {
-                                  return scale_y(locations_stations[d.station2.trim()][0]) + adjustment;
-                                }
-                                else
-                                {
-                                  return scale_y(locations_stations[d.station2.trim()][0]);
-                                }
-                              }
-                           })
-  .attr("stroke-width", width)
-  .attr("stroke", function(d){ if (d.nextTrajectory=="True")
-                               {
-                                 counter = counter + 1
-                               }
-                              return colours[counter]
-                             })
-
-console.log(connectionCoordinates)
 
 d3.selectAll("line")
   .on("mouseover", function(d){
@@ -180,10 +211,7 @@ d3.selectAll("line")
               tipConnections.hide(d);
           })
   
-
+  }
+}
     
 
-
-})
-
-}
